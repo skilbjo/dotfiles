c9ace50d8cfc7955f3cb45bf3ebb631efab075e5
John Skilbeck <john.skilbeck@gmail.com>
2016-06-25 19:02:30 -0700

    add install script

--- install.sh
+++ install.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 
+folder="~/Dropbox/"
+bashrc=".bashrc"
+profile=".profile"
+gitcfg=".gitconfig"
+vimrc=".vimrc"
+
+
+cd ~
+
+rm $bashrc ; rm $profile ; rm $gitcfg ; rm $vimrc
+
+ln -s $folder$bashrc $bashrc
+ln -s $folder$profile $profile
+ln -s $folder$vimrc $vimrc
+ln -s $folder$gitcfg $gitcfg
+ln -s $folder$gitign $gitign
 
 
