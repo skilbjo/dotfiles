113cbc79a8778ef6fe99b21526d14450b312925f
John Skilbeck <john.skilbeck@gmail.com>
2016-06-27 10:18:42 -0700

    clojure profiles

---
+++ profiles.clj
@@ -0,0 +1,7 @@
+{:user
+	{	:dependencies [[org.clojure/tools.namespace "0.2.3"]
+									[spyscope "0.1.3"]
+									[criterium "0.4.1"]]
+		:injections		[(require '(clojure.tools.namespace repl find))
+									(try (require 'spyscope.core)
+										(catch RuntimeException e))]}}
