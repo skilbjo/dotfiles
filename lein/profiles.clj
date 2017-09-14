49abf289f586ade2031d88aabcf376717dec38e0
John Skilbeck <john.skilbeck@gmail.com>
2017-09-14 11:12:21 -0700

    stuff

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,14 +1,13 @@
 {:user
- {:dependencies [[org.clojure/tools.namespace "0.2.11"]
-                 [jonase/eastwood "0.2.3" :exclusions  [org.clojure/clojure]]
+ {:dependencies [[org.clojure/tools.namespace "0.2.11"]     ; (refresh) in repl
+                 [cljfmt "0.5.7"]
+                 [io.aviso/pretty "0.1.34"]
+                 [jonase/eastwood "0.2.4" :exclusions  [org.clojure/clojure]]]
+  :plugins      [;[jonase/eastwood "0.2.4" :exclusions  [org.clojure/clojure]]
+                 [cider/cider-nrepl "0.15.0"]
                  [cljfmt "0.5.6"]
-                 [io.aviso/pretty "0.1.33"]
-                 [criterium "0.4.4"]]
-  :plugins      [[cider/cider-nrepl "0.13.0"]
-                 [lein-autoreload "0.1.1"]
-                 [cljfmt "0.5.6"]
-                 [io.aviso/pretty "0.1.33"]
-                 ;[jonase/eastwood "0.2.3" :exclusions  [org.clojure/clojure]]
+                 [io.aviso/pretty "0.1.34"]
                  [lein-auto "0.1.2"]
+                 [lein-autoreload "0.1.1"]
                  [venantius/ultra "0.5.1"]]}
  :repl-options {:init (require 'cljfmt.core)}}
